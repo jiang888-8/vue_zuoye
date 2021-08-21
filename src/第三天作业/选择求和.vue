7644c28930e2d0d7081b78026cb1dfd88d12d1a6
jiangdou <1565133920@qq.com>
2021-08-21 12:11:14 +0800

    求和完成

---
+++ src/第三天作业/选择求和.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div style="display: inline-block" v-for="(item,index) in arr" :key="index">
+      <input type="checkbox" v-model="newArr" :value="item">
+      <span> {{item}} </span>
+    </div>
+    <p>你选中的元素, 累加的值和为: {{sum}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      arr:[9, 15, 19, 25, 29, 31, 48, 57, 62, 79, 87],
+      newArr:[]
+    }
+  },
+
+  computed: {
+    sum () {
+      //使用reduce，必须要在后面加循环一次的时间
+      return this.newArr.reduce((num,value)=>{
+        return num += value
+      },0)
+    }
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
